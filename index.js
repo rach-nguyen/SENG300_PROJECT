109ffbca88df52596352aabd7a11f6ef9f68266e
Jack Yang <ahfhafh@gmail.com>
2020-03-22 15:12:46 -0600

    some changes

--- index.js
+++ index.js
@@ -1,13 +1,50 @@
-function getLoginInfo(){
-    let user = document.getElementById("uname").value;
-    console.log(user);
-    let pass = document.getElementById("passw").value;
-    console.log(pass);
-}
+var modal = document.getElementById("id01");
+var modal2 = document.getElementById("id02");
 
-function openSign() {
-    document.getElementById("signup").style.display = "block";
-}
-function closeSign() {
-    document.getElementById("signup").style.display = "none";
-}
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal || event.target == modal2) {
+    modal.style.display = "none";
+    modal2.style.display = "none";
+  }
+};
+
+var password = document.getElementById("psw");
+var confirmPassword = document.getElementById("Cpsw");
+var matchpassword = document.getElementById("matchpassword");
+var submit = document.getElementById("submit");
+
+password.onfocus = function() {
+  document.getElementById("checkpasswordmatch").style.display = "block";
+};
+
+password.onblur = function() {
+  document.getElementById("checkpasswordmatch").style.display = "none";
+};
+
+confirmPassword.onfocus = function() {
+  document.getElementById("checkpasswordmatch").style.display = "block";
+};
+
+confirmPassword.onblur = function() {
+  document.getElementById("checkpasswordmatch").style.display = "none";
+};
+
+submit.onfocus = function() {
+  document.getElementById("checkpasswordmatch").style.display = "block";
+};
+
+var check = function() {
+  if (
+    document.getElementById("psw").value ==
+    document.getElementById("Cpsw").value
+  ) {
+    document.getElementById("matchpassword").style.color = "green";
+    document.getElementById("matchpassword").innerHTML = "matching";
+    matchpassword.classList.remove("invalid");
+    matchpassword.classList.add("valid");
+  } else {
+    document.getElementById("matchpassword").style.color = "red";
+    document.getElementById("matchpassword").innerHTML = "not matching";
+  }
+};
--- registration.php
+++ registration.php
@@ -6,30 +6,26 @@ $_SESSION['message'] = '';
 $mysqli = new mysqli('localhost', 'root', '', 'seng');
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    //two passwords are equal to each other
-    if ($_POST['psw'] == $_POST['Cpsw']) {
 
-        $username = $mysqli->real_escape_string($_POST['uname']);
-        $email = $mysqli->real_escape_string($_POST['email']);
-        $password = md5($_POST['psw']);
-        
-        $_SESSION['uname'] = $username;
-        
-        $sql = "INSERT INTO users (email, username, password) "
-                . "VALUES ('$email', '$username', '$password')";
+    $username = $mysqli->real_escape_string($_POST['uname']);
+    $email = $mysqli->real_escape_string($_POST['email']);
+    $password = md5($_POST['psw']);
+    
+    $_SESSION['uname'] = $username;
+    
+    $sql = "INSERT INTO users (email, username, password) "
+            . "VALUES ('$email', '$username', '$password')";
 
-        // if the query is successful redirect to page.
-        if ($mysqli->query($sql) === true) {
-            $_SESSION['message'] = 'Registration succesful!';
-            header("location: welcome.html");
-        }
-        else {
-            $_SESSION['message'] = "User could not be added to the database.";
-        }
+    // if the query is successful redirect to page.
+    if ($mysqli->query($sql) === true) {
+        echo $_SESSION['message'] = "Registration succesful! Redirecting...";
+        header("refresh:2;url= welcome.html");
     }
     else {
-        $_SESSION['message'] = "Two passwords must match.";
+        echo $_SESSION['message'] = "User could not be added to the database. Redirecting...";
+        header("refresh:2;url= index.html");
     }
 }
 
 ?>
+
